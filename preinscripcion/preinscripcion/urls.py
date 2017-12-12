b13c49e25272c2b855ccdf4697e6301a29a0fdae
Alan Flores <borreh.2814@gmail.com>
2017-12-11 23:39:15 -0800

    Remove unnecessary files

--- preinscripcion/preinscripcion/urls.py
+++ preinscripcion/preinscripcion/urls.py
@@ -1,26 +1,6 @@
-"""preinscripcion URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include, url
 from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
-from home import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 router = routers.DefaultRouter()
@@ -34,22 +14,5 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
-    # url(r'^go/pick/$', views.pick, name='pick_view'),
-    # url(r'^login/$', views.login, name='login_view'),
-    # url(r'^logout/$', views.logout, name='logout_view'),
-    # url(r'^go/post/$', views.materiasPost, name='post_view'),
-    # url(r'', views.login, name='login_view'),
-
-
-    # url(r'^getLogin/$',views.login,name='getlogin')
-    # url(r'especialidades/',views.EspecialidadView.as_view(),name='especialidad_view'),
-    # url(r'materiasInfo/',views.MateriaInfoView.as_view(),name='materiaInfo_view'),
-    # url(r'materias/',views.MateriaView.as_view(),name='materia_view'),
-    # url(r'alumnos/',views.AlumnoView.as_view(),name='alumno_view'),
-    # url(r'avance/',views.AvanceMateriaView.as_view(),name='avance_view'),
-    # url(r'read/',views.ReadXls,name='read')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^api/', include(router.urls))
+]
