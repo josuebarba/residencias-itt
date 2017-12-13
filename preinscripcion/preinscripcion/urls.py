74dbef35ffeb0101b5a5508ce02e834431abe01e
josuebarba <josue.barba16@tectijuana.edu.mx>
2017-12-13 15:11:19 -0800

    Update urls.py

--- preinscripcion/preinscripcion/urls.py
+++ preinscripcion/preinscripcion/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-# router.register(r'count',views.CountViewSet,base_name="count")
-# router.register(r'especialidades',views.EspecialidadView,base_name="especialidades")
-# router.register(r'materiasInfo',views.MateriaInfoView,base_name="materiasInfo")
-# router.register(r'materias',views.MateriaView,base_name="materias")
-# router.register(r'alumnos',views.AlumnoView,base_name="alumnos")
-# router.register(r'avance',views.AvanceMateriaView,base_name="avance")
-# router.register(r'especialidades',views.EspecialidadView,base_name="especialidadview")
+router.register(r'count',views.CountViewSet,base_name="count")
+router.register(r'especialidades',views.EspecialidadView,base_name="especialidades")
+router.register(r'materiasInfo',views.MateriaInfoView,base_name="materiasInfo")
+router.register(r'materias',views.MateriaView,base_name="materias")
+router.register(r'alumnos',views.AlumnoView,base_name="alumnos")
+router.register(r'avance',views.AvanceMateriaView,base_name="avance")
+router.register(r'especialidades',views.EspecialidadView,base_name="especialidadview")
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
